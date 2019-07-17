a81885cd074ec4f899e5b968c3ec13ed6adf5281
Chris Shreve <cshreve@rei.com>
2019-07-17 16:02:18 -0700

    project stuff

---
+++ Example/Pods/Target Support Files/FooLib/FooLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FooLib : NSObject
+@end
+@implementation PodsDummy_FooLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-FooLib_Example/Pods-FooLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FooLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FooLib_Example
+@end
